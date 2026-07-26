b7767a977667937153f47e55e7484545b7194911
Aidan Aoun <aidanaaoun@gmail.com>
2026-07-26 01:12:17 -0700

    Added tensor results to arrays transformation function

--- object_detection/object_detection/object_detector_node.py
+++ object_detection/object_detection/object_detector_node.py
@@ -18,18 +18,57 @@ class ObjectDetectionNode(Node):
         self.detection_results = None
         self.image_bridge = CvBridge()
         self.vision_model = YOLO("yolo26n.pt")
-
-        
         self.camera_subscription = self.create_subscription(Image, '/camera/image_raw', self.camera_callback, 10)
 
 
     def camera_callback(self, msg):
         self.curr_image_cv = self.image_bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
         self.detection_results = self.vision_model.predict(self.curr_image_cv, conf=0.5)
-        semantic_image = self.detection_results[0].plot()
-        cv.imshow("Decection Image", semantic_image)
-        cv.waitKey(10)
+        curr_detection_result = self.detection_results[0]
 
+        cv.imshow("image", curr_detection_result.plot())
+        cv.waitKey(1)
+
+        box_coords, box_names = self.result_tensors_to_arrays(curr_detection_result)
+        if box_coords is None or box_names is None:
+            return
+        
+        self.localize_objects()
+        self.add_object_to_map()
+        return
+
+
+    def result_tensors_to_arrays(self, detection_result):
+        boxes = detection_result.boxes
+        if boxes is None or len(boxes) == 0:
+            self.get_logger().info("No objects detected")
+            return [None, None]
+        
+        boxes_tensor = boxes.xyxy
+        class_ids_tensor = boxes.cls
+        box_count = boxes_tensor.size(dim=0)
+        box_coords = np.ndarray((box_count, 4))
+        box_names = []
+        
+        for i in range(box_count):
+            box_coords[i][0] = boxes_tensor[i, 0].item()
+            box_coords[i][1] = boxes_tensor[i, 1].item()
+            box_coords[i][2] = boxes_tensor[i, 2].item()
+            box_coords[i][3] = boxes_tensor[i, 3].item()
+
+            cur_class_id = int(class_ids_tensor[i].item())
+            box_names.append(detection_result.names[cur_class_id])
+            self.get_logger().info(f" Box coordinates {box_names[i]}: x1={box_coords[i][0]}, y1={box_coords[i][1]}, {box_coords[i][2]}, {box_coords[i][3]}")
+
+        box_names = np.array(box_names)
+        return [box_coords, box_names]
+
+
+    def localize_objects(self):
+        return
+
+
+    def add_object_to_map(self):
         return
 
 
