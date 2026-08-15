816ea44dbb92e865ae8e20a43a2227203a7b924a
aidan aoun <aidanaaoun@gmail.com>
2026-08-14 23:49:43 -0400

    added all neccesary nodes to physical launch

--- bringup/launch/recon_car_physical.launch.py
+++ bringup/launch/recon_car_physical.launch.py
@@ -8,6 +8,9 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 from launch.substitutions import PathJoinSubstitution
+from launch.event_handlers import OnProcessExit
+from launch.actions import RegisterEventHandler
+from launch.actions import IncludeLaunchDescription, AppendEnvironmentVariable, TimerAction, ExecuteProcess
 
 
 def generate_launch_description():
@@ -55,11 +58,42 @@ def generate_launch_description():
             os.path.join(get_package_share_directory('slam_toolbox'), 'launch', 'online_async_launch.py')),
         launch_arguments={'slam_params_file': os.path.join(pkg_share, 'params', 'mapper_params_online_async.yaml')}.items())
 
+    nav2_launch_object = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('nav2_bringup'), 'launch', 'navigation_launch.py')),
+        launch_arguments={'use_sim_time': 'true', 'params_file': os.path.join(pkg_share, 'params', 'nav2_params.yaml')}.items())
+
+    slam_and_nav2_delayed_launch = RegisterEventHandler(
+        OnProcessExit(
+            target_action= diff_controller_spawn_node,
+            on_exit=[slam_launch_object, TimerAction(period=2.0, actions=[nav2_launch_object])],))
+    
+    goal_pose_node = Node(
+        package = 'goal_pose_creation',
+        executable = 'goal_pose_node',
+        arguments = [])
+    
+    delayed_goal_pose_spawner = TimerAction(
+        period=20.0,
+        actions=[goal_pose_node])
+
+    object_detection_node = Node(
+        package = 'object_detection', 
+        executable = 'object_detector',
+        arguments = [])
+
+    camera_node = Node(
+        package = 'camera_ros',
+        executable = 'camera_node',
+        arguments = [])
 
     ld.add_action(robot_state_pub_node)
     ld.add_action(controller_manager)
     ld.add_action(diff_controller_spawn_node)
     ld.add_action(joint_broadcaster_spawn_node)
     ld.add_action(rplidar_composition_node)
-    ld.add_action(slam_launch_object)
+    ld.add_action(slam_and_nav2_delayed_launch)
+    ld.add_action(delayed_goal_pose_spawner)
+    ld.add_action(camera_node)
+    # ld.add_action(object_detection_node)
     return ld
